5b69086245c7b9989777a364f4a0b1701c2b4c5c
Jacob Paul <jacobpaul787@gmail.com>
2017-09-05 14:27:14 -0600

    Game Dao Impl filled out

--- N-BackREST/src/data/GameDAO.java
+++ N-BackREST/src/data/GameDAO.java
@@ -8,11 +8,4 @@ public interface GameDAO {
 	public Set<Game> indexGame();
 
 	public Game showGame(Integer gameId);
-
-	public Game createGame(String gameJson);
-
-	public Game updateGame(Integer gameId, String gameJson);
-
-	public Boolean destroyGame(Integer gameId);
-
 }
--- N-BackREST/src/data/GameDAOImpl.java
+++ N-BackREST/src/data/GameDAOImpl.java
@@ -1,45 +1,44 @@
 package data;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import entities.Challenge;
 import entities.Game;
 
+@Transactional
+@Repository
 public class GameDAOImpl implements GameDAO {
 
 	@PersistenceContext
 	private EntityManager em;
-	
+
 	@Override
 	public Set<Game> indexGame() {
-		// TODO Auto-generated method stub
-		return null;
+		String query = "SELECT c FROM Challenge c";
+		List<Game> gameList = em.createQuery(query, Game.class)
+				.getResultList();
+
+		Set<Game> games = new HashSet<Game>(gameList);
+		return games;
 	}
 
 	@Override
 	public Game showGame(Integer gameId) {
-		// TODO Auto-generated method stub
-		return null;
+		String query = "SELECT c FROM Challenge c WHERE c.id=:id";
+		Game game = em.createQuery(query, Game.class)
+				.setParameter("id", gameId)
+				.getResultList()
+				.get(0);
+		return game;
 	}
-
-	@Override
-	public Game createGame(String gameJson) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Game updateGame(Integer gameId, String gameJson) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Boolean destroyGame(Integer gameId) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	
+	
 }
