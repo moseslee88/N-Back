539ed27fd4d32979da4c44c051663ad0fc2e087b
Moses Lee <moseslee88@gmail.com>
2017-09-05 09:34:51 -0600

    added AuthDAO and NbackDAO

--- N-BackJPA/src/entities/Challenge.java
+++ N-BackJPA/src/entities/Challenge.java
@@ -1,5 +1,63 @@
 package entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Challenge {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user; 
+
+	@Column(name = "result_id")
+	private Integer resultId;
+
+	@Column(name = "target_user_id")
+	private Integer targetUserId;
+
+	// gets and sets
+
+	public Integer getId() {
+		return id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Integer getResultId() {
+		return resultId;
+	}
+
+	public void setResultId(Integer resultId) {
+		this.resultId = resultId;
+	}
+
+	public Integer getTargetUserId() {
+		return targetUserId;
+	}
+
+	public void setTargetUserId(Integer targetUserId) {
+		this.targetUserId = targetUserId;
+	}
+
+	@Override
+	public String toString() {
+		return "Challenge [id=" + id + ", resultId=" + resultId + ", targetUserId=" + targetUserId + "]";
+	}
 
 }
