23b7c81f62510d5e49504ecfeba1e05e2b57c14c
mmJava <maldojavadev@gmail.com>
2017-09-05 10:26:40 -0600

    Profile complete

--- N-BackJPA/src/entities/Profile.java
+++ N-BackJPA/src/entities/Profile.java
@@ -5,10 +5,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Profile {
 	
+	@OneToOne
+	@JoinColumn(name="user_id")
 	private User user;
 	
 	@Id
