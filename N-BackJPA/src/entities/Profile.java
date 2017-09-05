be4ae10c397e6c502d93ab6718dce268f16aba43
mmJava <maldojavadev@gmail.com>
2017-09-05 09:50:31 -0600

    updated profile entity

--- N-BackJPA/src/entities/Profile.java
+++ N-BackJPA/src/entities/Profile.java
@@ -1,13 +1,119 @@
 package entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Profile {
 	
+	private User user;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
 	private String name;
+	
+	@Column(name="birth_year")
 	private Integer birthYear;
+	
 	private Character gender;
 	
+	private String city;
+	
+	private String region;
+	
+	private String education;
+	
+	private String country;
+	
+	@Column(name="household_income")
+	private Double householdIncome;
+	
+	public User getUser() {
+		return user;
+	}
+	
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	public Integer id() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Integer getBirthYear() {
+		return birthYear;
+	}
+	
+	public void setBirthYear(Integer birthYear) {
+		this.birthYear = birthYear;
+	}
+	
+	public Character getGender() {
+		return gender;
+	}
+	
+	public void setGender(Character gender) {
+		this.gender = gender;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+	
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	public String getRegion() {
+		return region;
+	}
+	
+	public void setRegion(String region) {
+		this.region = region;
+	}
+	
+	public String getEdu() {
+		return education;
+	}
+	
+	public void setEdu(String edu) {
+		this.education = edu;
+	}
+	
+	public String getCountry() {
+		return country;
+	}
+	
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	
+	public Double getHouseholdIncome() {
+		return householdIncome;
+	}
+	
+	public void setHouseholdIncome(Double householdIncome) {
+		this.householdIncome = householdIncome;
+	}
+
 	
 
 }
