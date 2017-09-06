e4a54d073bb70fc941fbda91c781c76c44ef10b4
Jacob Paul <jacobpaul787@gmail.com>
2017-09-06 10:01:55 -0600

    Result controller updated

--- N-BackREST/src/controllers/ResultController.java
+++ N-BackREST/src/controllers/ResultController.java
@@ -1,5 +1,46 @@
 package controllers;
 
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import data.ResultDAO;
+import data.UserDAO;
+import entities.Challenge;
+import entities.Result;
+
+@RestController
 public class ResultController {
+
+	@Autowired
+	private UserDAO udao;
+	@Autowired
+	private ResultDAO dao;
+	
+	@RequestMapping(path = "ping", method = RequestMethod.GET)
+	public String ping() {
+		return "pong";
+	}
+	
+	@RequestMapping(path = "/user/{uid}/result", method = RequestMethod.GET)
+	public Set<Result> index(HttpServletRequest req, HttpServletResponse res, @PathVariable int uid) {
+		return dao.indexResult(uid);
+	}
+	@RequestMapping(path = "/user/{uid}/result/{rid}", method = RequestMethod.GET)
+	public Result show(HttpServletRequest req, HttpServletResponse res, @PathVariable int uid, @PathVariable int rid) {
+		return dao.showResult(uid, rid);
+	}
+	@RequestMapping(path = "/user/{uid}/result", method = RequestMethod.POST)
+	public Result create(HttpServletRequest req, HttpServletResponse res, @PathVariable int uid, @RequestBody String resultJson) {
+		return dao.createResult(uid, resultJson);
+	}
 
 }
