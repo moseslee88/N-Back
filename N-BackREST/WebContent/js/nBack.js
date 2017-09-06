b96fe3fa27ddb2505204b7cf97e2f8c7dba5396e
csgibson4 <csgibson4@gmail.com>
2017-09-06 13:28:17 -0600

    serv fix bcrypt bean

--- N-BackREST/WebContent/js/nBack.js
+++ N-BackREST/WebContent/js/nBack.js
@@ -1,6 +1,14 @@
 //n-back test logic here
+var run;
 
+$(document).ready(function() {
+  run();
+});
 
+run = function() {
+  var numArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-var numArr = [0,1,2,3,4,5,6,7,8,9];
+  for (var i = 0; i < 60; i++) {
 
+  }
+}
