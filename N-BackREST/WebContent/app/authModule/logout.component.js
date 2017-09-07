304a74c90c5b5431be9e14254f2e4a923567eeb4
Moses Lee <moseslee88@gmail.com>
2017-09-07 14:44:17 -0600

    remove user id cookies in logout component

--- N-BackREST/WebContent/app/authModule/logout.component.js
+++ N-BackREST/WebContent/app/authModule/logout.component.js
@@ -1,12 +1,13 @@
 angular.module('authModule')
 .component('logout', {
 	templateUrl : 'app/authModule/logout.component.html',
-	controller : function(authService, $location) {
+	controller : function(authService, $location, $cookies) {
 		var vm = this;
 		
 		vm.logout = function() {
 			authService.logout()
 				.then(function(res){
+					$cookies.remove('uid');
 					$location.path("/")
 				})
 				.catch(console.error)
