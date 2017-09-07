e55c8221d8f95cdf0858f2fe0bc1802a7f44fddc
csgibson4 <csgibson4@gmail.com>
2017-09-07 14:42:33 -0600

    game comp

--- N-BackREST/WebContent/app/gameModule/game.component.js
+++ N-BackREST/WebContent/app/gameModule/game.component.js
@@ -1,11 +1,21 @@
 angular.module('gameModule')
-.component('game', {
-	templateUrl : 'app/gameModule/game.component.html',
-	controller : function( $location) {
-		var vm = this;
-		console.log("we are in the game component")
-		
-		
-	},
-	controllerAs  : 'vm'
-})
+	.component('game', {
+		templateUrl : 'app/gameModule/game.component.html',
+		controller : function($location, gameService) {
+			var vm = this;
+			console.log("we are in the game component")
+
+			vm.gameList = [];
+			vm.getGameList = function() {
+				gameService.index()
+					.then(function(res) {
+						vm.gameList = (res.data);
+					})
+					.catch(console.error);
+			}
+			vm.getGameList();
+			console.log(vm.gameList);
+
+		},
+		controllerAs : 'vm'
+	})
