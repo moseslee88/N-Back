be632f3da1ab5134005e622408afea1a79c0a21a
mmJava <maldojavadev@gmail.com>
2017-09-07 13:51:55 -0600

    added profile comp and pprofilen js

--- N-BackREST/WebContent/app/appModule/profile.component.js
+++ N-BackREST/WebContent/app/appModule/profile.component.js
@@ -1,5 +1,13 @@
 angular.module('appModule')   
     .component('profile', {
-    	templateUrl: 'app/appModule/profile.component.html'
+    	templateUrl: 'app/appModule/profile.component.html',
+    	
+    		controller : function($location){
+    			var vm = this;
+    			
+    			
+    		},
+    
+    controllerAs : 'vm'
 
     })
