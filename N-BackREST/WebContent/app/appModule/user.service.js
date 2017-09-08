650c12237a2f20962be7ae8e91f29b5b8b8311b4
Jacob Paul <jacobpaul787@gmail.com>
2017-09-08 10:16:18 -0600

    added user service

---
+++ N-BackREST/WebContent/app/appModule/user.service.js
@@ -0,0 +1,58 @@
+angular.module('appModule')
+.factory('userService', function($filter, $http, authService, $rootScope, $cookies) {
+  var service = {};
+  var userId = $cookies.get("uid");
+
+		service.index = function() {
+			return $http({
+				method : 'GET',
+				url : 'api/user'
+			})
+		};
+
+		service.show = function() {
+			return $http({
+				method : 'GET',
+				url : 'api/user/' + authService.getToken().id 
+
+			})
+		};
+
+		service.create = function(user) {
+			var newUser = angular.copy(user);
+
+			return $http({
+				method : 'POST',
+				url : 'api/user',
+				headers : {
+					'Content-Type' : 'application/json'
+				},
+				data : newUser
+			})
+				.then(function(res) {
+					$rootScope.$broadcast('created', {
+						profile : res.data
+					})
+					return res;
+				})
+		};
+
+		service.update = function(user) {
+			return $http({
+				method : 'PUT',
+				url : 'api/user/' + authService.getToken().id,
+				headers : {
+					'Content-Type' : 'application/json'
+				},
+				data : user
+			})
+		},
+
+		service.destroy = function() {
+			return $http({
+				method : 'DELETE',
+				url : 'api/user/' + authService.getToken().id
+			})
+		};
+		return service;
+	})
