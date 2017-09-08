3a3784fb13f3039c87984293f3129cb36e417724
Jacob Paul <jacobpaul787@gmail.com>
2017-09-08 10:30:32 -0600

    removed console logs

--- N-BackREST/WebContent/app/appModule/home.component.js
+++ N-BackREST/WebContent/app/appModule/home.component.js
@@ -1,16 +1,21 @@
 angular.module('appModule')
    .component('home', {   
 	   templateUrl: 'app/appModule/home.component.html',
-	  controller: function($location, $cookies, resultService, profileService, gameService, challengeService)  {
+	  controller: function($location, $cookies, resultService, profileService, gameService, challengeService, userService)  {
 		   var vm = this;
 		   vm.currentUserId = null;
+		   vm.currentUser = null; 
 		   
-		   vm.getCurrentUser = function(){
+		   vm.getCurrentUserID = function(){
 			   vm.currentUserId = $cookies.get("uid");
 		   }
-		   vm.getCurrentUser();
+		   vm.getCurrentUserID();
 		   
-		   console.log(vm.currentUserId);
+		   vm.getUser = function(){
+			  vm.currentUser = userService.show();
+		   }
+		   vm.getUser();
+		   console.log(vm.currentUser.id);
 		   
 		   vm.goToRegister = function(){
 			   $location.path('/register')
--- N-BackREST/WebContent/app/appModule/navigator.component.js
+++ N-BackREST/WebContent/app/appModule/navigator.component.js
@@ -11,8 +11,6 @@ angular.module('appModule')
 				vm.currentUserId = $cookies.get("uid");
 			}
 			vm.getCurrentUser();
-			console.log(vm.currentUserId);
-			
 
 			vm.logout = function() {
 				authService.logout().then(function(response) {
