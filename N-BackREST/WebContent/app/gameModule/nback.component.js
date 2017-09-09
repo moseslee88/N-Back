b1709c5c8c0dd8c926fc7a7102d853ae661bd78f
csgibson4 <csgibson4@gmail.com>
2017-09-09 11:09:55 -0600

    n-back half done, game comp will display correct game

--- N-BackREST/WebContent/app/gameModule/nback.component.js
+++ N-BackREST/WebContent/app/gameModule/nback.component.js
@@ -1,11 +1,76 @@
 angular.module('gameModule')
-.component('nback', {
-	templateUrl : 'app/gameModule/nback.component.html',
-	controller : function( $location, $timeout) {
-		var vm = this;
-		
-		
-		
-	},
-	controllerAs  : 'vm'
-})
+	.component('nback', {
+		templateUrl : 'app/gameModule/nback.component.html',
+		controller : function(randomNumService, $timeout, $interval) {
+			var vm = this;
+			//various fields that will be var instead of vm eventually
+			vm.fullArr = [];
+			vm.movingArr = [];
+			vm.currentIndex = 0;
+			vm.displayNum = -1;
+			vm.difficulty = 1;
+			vm.points = 0;
+
+			//reset game so nothing bleeds over from a previous play
+			vm.setUpGame = function(diff) {
+				vm.fullArr = randomNumService.getNums(5, (30 + diff));
+				console.log(vm.fullArr)
+				vm.displayNum = -1;
+				vm.currentIndex = 0;
+				vm.points = 0;
+				vm.difficulty = diff;
+			}
+			
+			//doesn't work! will display the current comparison number for n-back game
+			vm.getDisplayNum = function(){
+				return vm.displayNum;
+			}
+
+			//increments the index to go through the array and gets the array that will be used to check if the user is correct
+			vm.runLoop = function() {
+				vm.displayNum = vm.movingArr[vm.currentIndex];
+				vm.movingArr = vm.fullArr.slice((vm.currentIndex - vm.difficulty - 1), vm.currentIndex);
+				vm.currentIndex++;
+				console.log("moving: " + vm.movingArr);
+			}
+
+			//user clicked the button, now either add points or take them away (points are not normalized to 100 per game yet)
+			vm.checkCorrect = function() {
+				console.log("checkCorrect");
+				if (vm.movingArr.length) {
+					if (vm.movingArr[0] === vm.movingArr[(vm.movingArr.length-1)]) {
+						console.log("correct");
+						vm.points += 100;
+					} else {
+						vm.points -= 2;
+					}
+				}
+			}
+
+			//show number of points earned in the console
+			vm.showResult = function() {
+				console.log("points: " + vm.points);
+			}
+
+			//use $interval to iteratively run the game loop until there are no numbers left in the array
+			vm.runGame = function(diff) {
+				$interval(vm.runLoop, 1500, vm.fullArr.length)
+					.then(function() {
+						vm.showResult();
+					}, function(e) {
+						console.log(e);
+					});
+			}
+			
+			//user clicks start button which calls all other functions, not sure that I am using $timeout correctly
+			vm.startGame = function(diff){	
+				$timeout(vm.setUpGame(diff), 3000)
+				.then(function(d) {
+					vm.runGame(diff);
+				}, function(e) {
+					console.log(e);
+				})
+			}
+		},
+		controllerAs : 'vm'
+	})
--- N-BackREST/WebContent/app/gameModule/randomNum.service.js
+++ N-BackREST/WebContent/app/gameModule/randomNum.service.js
@@ -1,11 +1,11 @@
 //this is broken!
 
 angular.module('gameModule')
-	.factory('randomNumService', function($filter, $http, $rootScope) {
+	.factory('randomNumService', function() {
 		var service = {};
 
 		var digits = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
-		
+
 		//deprecated  V
 		//input number of digits needed, will return array of digits
 		service.getDigits = function(inputNum) {
@@ -15,16 +15,17 @@ angular.module('gameModule')
 			}
 			return finalArr;
 		};
-		
+
 		//preferred  V
 		//input max num (exclusive!) and total nums needed, will return array of random nums
-		service.getNums = function(maxNum, totalNum){
+		service.getNums = function(maxNum, totalNum) {
+			console.log("randomNumservice.getNums");
 			finalArr = [];
 			for (var i = 0; i < totalNum; i++) {
-				finalArr.push(Math.floor(Math.random()*maxNum));
+				finalArr.push(Math.floor(Math.random() * maxNum));
 			}
 			return finalArr;
-		}
+		};
 
 		return service;
 	})
