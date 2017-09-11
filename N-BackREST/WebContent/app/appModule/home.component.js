5ff0ceeb2b200d1cd3da05210a877ae809b059d9
csgibson4 <csgibson4@gmail.com>
2017-09-11 09:50:02 -0600

    stupid jake

--- N-BackREST/WebContent/app/appModule/home.component.js
+++ N-BackREST/WebContent/app/appModule/home.component.js
@@ -1,42 +1,45 @@
 angular.module('appModule')
-   .component('home', {   
-	   templateUrl: 'app/appModule/home.component.html',
-	  controller: function($location, $cookies, resultService, profileService, gameService, challengeService, userService, authService, $scope)  {
-		   var vm = this;
-		   vm.currentUserId = null;
-		   vm.currentUser = null; 
-		   
-		   vm.getUser = function(){
-			   if (authService.getToken().id) {
-				   userService.show()
-			   		.then(function(res){
-			   		vm.currentUser = res.data;
-			   		console.log(vm.currentUser);
-			   	});
-			   }
-		   }
-		   
-		   vm.getCurrentUserID = function(){
-			   vm.currentUserId = $cookies.get("uid");
-			   vm.getUser();
-		   }
-		   vm.getCurrentUserID();
-		   
-		   vm.goToRegister = function(){
-			   $location.path('/register')
-		   };
-		   vm.goToProfile = function(){
-			   console.log("in gotoprofile")
-			   $location.path('/profile')
-		   };
-		   
-		   var listenForLogin = function(){
-			   vm.getUser();
-			   vm.getCurrentUserID();
-		   }
-		   
-		   $scope.$on('login', listenForLogin);
-		   
-	   },
-	   controllerAs: 'vm'
-   })
+	.component('home', {
+		templateUrl : 'app/appModule/home.component.html',
+		controller : function($location, $cookies, resultService, profileService, gameService, challengeService, userService, authService, $scope) {
+			var vm = this;
+			vm.currentUserId = null;
+			vm.currentUser = null;
+
+			vm.getUser = function() {
+				if (authService.getToken().id) {
+					userService.show()
+						.then(function(res) {
+							vm.currentUser = res.data;
+							console.log(vm.currentUser);
+						});
+				}
+			}
+
+			vm.getCurrentUserID = function() {
+				vm.currentUserId = $cookies.get("uid");
+				vm.getUser();
+			}
+			vm.getCurrentUserID();
+
+			vm.goToRegister = function() {
+				$location.path('/register');
+			};
+			vm.goToProfile = function() {
+				console.log("in gotoprofile");
+				$location.path('/profile');
+			};
+			vm.goToGame = function() {
+				$location.path('/game');
+			}
+
+			var listenForLogin = function() {
+				vm.getUser();
+				vm.getCurrentUserID();
+			}
+
+			$scope.$on('login', listenForLogin);
+
+		},
+		controllerAs : 'vm'
+	})
