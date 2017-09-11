9c76a496d2fe7d5b90f155a401d9c95b96309d94
Moses Lee <moseslee88@gmail.com>
2017-09-11 07:28:00 -0600

    stubbing out motest.html

--- N-BackREST/WebContent/app/gameModule/motest.component.js
+++ N-BackREST/WebContent/app/gameModule/motest.component.js
@@ -1,9 +1,20 @@
 angular.module('gameModule')
 .component('motest', {
 	templateUrl : 'app/gameModule/motest.component.html',
-	controller : function($location) {
+	controller : function($location, $timeout, $rootScope) {
 		var vm = this;
 		
+		vm.ArrofNumbers = [];
+		
+		 $timeout(callAtTimeout, 3000);
+
+
+
+	function callAtTimeout() {
+	    console.log("Timeout occurred - just a time test");
+	}
+	
+	
 		
 		
 	},
