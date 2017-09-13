60a982f396c2c12621d10c24bef3610037c708df
Moses Lee <moseslee88@gmail.com>
2017-09-13 10:48:50 -0600

    repaired issue with slow login and issue with refresh after login

--- N-BackREST/WebContent/app/authModule/logout.component.js
+++ N-BackREST/WebContent/app/authModule/logout.component.js
@@ -1,17 +1,17 @@
 angular.module('authModule')
 .component('logout', {
 	templateUrl : 'app/authModule/logout.component.html',
-	controller : function(authService, $location, $cookies) {
-		var vm = this;
-		
-		vm.logout = function() {
-			authService.logout()
-				.then(function(res){
-					$cookies.remove('uid');
-					$location.path("/")
-				})
-				.catch(console.error)
-		}
+	controller : function(authService, $location, $cookies, $rootScope) {
+		var vm = this;		
+//		vm.logout = function() {
+//			authService.logout()
+//				.then(function(res){
+//					$rootScope.$broadcast('logout',{});
+//					$cookies.remove('uid');
+//					$location.path("/")
+//				})
+//				.catch(console.error)
+//		}
 		
 	},
 	controllerAs  : 'vm'
